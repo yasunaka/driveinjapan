a01d2a2a7254cd6c6cd90713e661993c36fe81d5
Kurita <kurita@jpctrade.com>
2016-08-26 11:53:47 +0900

    correct global-nav-for-cellphone

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,16 @@
 $(function() {
+//				var display = $('.nav-toggle').css("display");
 				$('.nav-toggle').click( function() {
-								$('.nav-item').slideToggle();
-								return false;
+								$('.global-nav-for-cellphone').slideToggle();
 				});
+
+});
+$(function() {
+				var width = $(document).width()
+				var theDropDown = document.querySelector('.global-nav');
+				if (width > 640 ) {
+								theDropDown.classList.add("display-block");
+				} else {
+								theDropDown.classList.remove("display-block");
+				}
 });
